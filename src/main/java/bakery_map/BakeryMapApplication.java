17d2f6e4076f1ac79b428c1cf6126731c43d6063
imssu <imssu@imssuui-MacBookPro.local>
2026-03-20 18:41:12 +0900

    init project

---
+++ src/main/java/bakery_map/BakeryMapApplication.java
@@ -0,0 +1,13 @@
+package bakery_map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakeryMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakeryMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bakery_map/BakeryMapApplicationTests.java
@@ -0,0 +1,13 @@
+package bakery_map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakeryMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
