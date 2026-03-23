f2909aca2efb641819434370a547309fcd6714c4
imssu <imssu@imssuui-MacBookPro.local>
2026-03-23 12:31:40 +0900

    fix: datasource 제거

--- src/main/java/bakery_map/BakeryMapApplication.java
+++ src/main/java/bakery_map/BakeryMapApplication.java
@@ -1,13 +1,27 @@
+// package bakery_map;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+// @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+// public class BakeryMapApplication {
+
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(BakeryMapApplication.class, args);
+// 	}
+// }
+
+
 package bakery_map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class BakeryMapApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BakeryMapApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BakeryMapApplication.class, args);
+    }
 }
